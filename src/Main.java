a3f2e4e6fb643a5bd22dec2b7e3dd59be0103c91
Quentin Marois <56384953+quentinmarois@users.noreply.github.com>
2023-04-01 20:10:49 +0200

    feat(MultiOutputStream) : redirect output to console and file (as required from subject)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+
+        // Setup trace file and outputs
+        FileOutputStream traceFile = new FileOutputStream("trace.txt", true);
+        traceFile.write("\n\n".getBytes());
+        MultiOutputStream multiOut = new MultiOutputStream(System.out, traceFile);
+        PrintStream printStream = new PrintStream(multiOut);
+        System.setOut(printStream);
+
         boolean run = true;
         while (run){
             try {
                 System.out.print("Enter constraint table to use (ex: table 1): ");
                 String table = scanner.nextLine();
+                traceFile.write((table + '\n').getBytes());
+
                 Graph graph = new Graph("src/tests/" + table + ".txt");
 
                 System.out.println("Reading graph from table (adding alpha and omega):");
@@ -43,6 +57,7 @@ public class Main {
             finally {
                 System.out.print("Do you want to continue? (y/n): ");
                 String answer = scanner.nextLine();
+                traceFile.write((answer + '\n').getBytes());
                 if (answer.equals("n")){
                     run = false;
                 }
---
+++ src/MultiOutputStream.java
@@ -0,0 +1,72 @@
+import java.io.IOException;
+import java.io.OutputStream;
+
+// This class is used to redirect the output of the console to multiple streams
+
+public class MultiOutputStream extends OutputStream
+{
+    OutputStream[] outputStreams;
+
+    public MultiOutputStream(OutputStream... outputStreams)
+    {
+        this.outputStreams= outputStreams;
+    }
+
+    @Override
+    public void write(int b) throws IOException
+    {
+        for (OutputStream out: outputStreams)
+            out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException
+    {
+        for (OutputStream out: outputStreams)
+            out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        for (int out = 0; out < outputStreams.length; out++ ) {
+
+            if (out == 0) {
+                // Writing to the console, don't filter out any bytes
+                outputStreams[0].write(b, off, len);
+            } else {
+                // Writing to the file, filter out escape sequences
+                int i = off;
+                int end = off + len;
+                while (i < end) {
+                    byte currentByte = b[i++];
+                    if (currentByte == 27) {
+                        // Found an escape sequence, ignore until the end of the sequence
+                        while (i < end) {
+                            currentByte = b[i++];
+                            if (currentByte == 'm') {
+                                break;
+                            }
+                        }
+                    } else {
+                        // Found a normal byte, write it to the file
+                        outputStreams[out].write(currentByte);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void flush() throws IOException
+    {
+        for (OutputStream out: outputStreams)
+            out.flush();
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        for (OutputStream out: outputStreams)
+            out.close();
+    }
+}
